b586587e981c7f04e5b42b622835eba5a8e548ef
Alexandra Yasnovskaya <a.yasnovskaya@gmail.com>
2025-08-16 20:18:41 +0300

    feat: nothing type

--- src/corporation/DirectorMain.kt
+++ src/corporation/DirectorMain.kt
@@ -1,14 +1,11 @@
 package corporation
 
 fun main() {
-    val assistant = WorkerRepository.findAssistant()
-    assistant?.work()
     val director = WorkerRepository.findDirector()
-    if (assistant != null) {
-        director?.takeCoffee(assistant)
-    }
+        ?: throwDirectorIsRequired()
+    director.printInfo()
+}
 
-    val directorSalary = director?.salary ?: 0
-    val assistantSalary = assistant?.salary ?: 0
-    val sum = directorSalary + assistantSalary
+fun throwDirectorIsRequired(): Nothing {
+    throw IllegalStateException("Director is required for this program. Please add it to file")
 }
