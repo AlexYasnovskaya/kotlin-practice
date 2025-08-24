a39fe25c30ab4f57a262b9af06dff7445a0bc616
Alexandra Yasnovskaya <a.yasnovskaya@gmail.com>
2025-08-24 13:51:25 +0300

    feat: extension functions

--- FunctionalProgramming/src/main/kotlin/products/Store.kt
+++ FunctionalProgramming/src/main/kotlin/products/Store.kt
@@ -3,32 +3,10 @@ package products
 fun main() {
     val products = ProductRepository.products
         .filter { it.productCategory == ProductCategory.CLOTHING }
-        .transform { it.copy(productPrice = it.productPrice * 2) }
-        .transform { "${it.id} - ${it.productName} - ${it.productPrice}" }
+        .map { it.copy(productPrice = it.productPrice * 2) }
+        .map { "${it.id} - ${it.productName} - ${it.productPrice}" }
 
     for (product in products) {
         println(product)
     }
-}
-
-fun List<Products>.filter(isSuitable: (Products) -> Boolean): List<Products> {
-    val result = mutableListOf<Products>()
-
-    for (product in this) {
-        if (isSuitable(product)) {
-            result.add(product)
-        }
-    }
-
-    return result.toList()
-}
-
-fun <T> List<Products>.transform(operation: (Products) -> T): List<T> {
-    val result = mutableListOf<T>()
-
-    for (product in this) {
-        result.add(operation(product))
-    }
-
-    return result.toList()
 }
--- FunctionalProgramming/src/main/kotlin/profile/Profile.kt
+++ FunctionalProgramming/src/main/kotlin/profile/Profile.kt
@@ -6,29 +6,9 @@ fun main() {
         .filter { it.firstName.startsWith("A") }
         .filter { it.gender == Gender.MALE }
         .filter { it.age < 30 }
-        .transform { it.copy(age = it.age + 1) }
+        .map { it.copy(age = it.age + 1) }
 
     for (profile in profiles) {
         println(profile)
     }
-}
-
-fun List<Person>.filter(isSuitable: (Person) -> Boolean): List<Person> {
-    val result = mutableListOf<Person>()
-
-    for (profile in this) {
-        if (isSuitable(profile)) {
-            result.add(profile)
-        }
-    }
-
-    return result.toList()
-}
-
-fun <T> List<Person>.transform( operation: (Person) -> T): List<T> {
-    val result = mutableListOf<T>()
-    for (profile in this) {
-        result.add(operation(profile))
-    }
-    return  result
 }
