6fded4445ffe27a1f352f4ccbb7c4c8b99dd7db4
Alexandra Yasnovskaya <a.yasnovskaya@gmail.com>
2025-08-24 18:08:46 +0300

    feat: transform dictionary.json to map obj & decode to map

--- FunctionalProgramming/src/main/kotlin/dictionary/Dictionary.kt
+++ FunctionalProgramming/src/main/kotlin/dictionary/Dictionary.kt
@@ -7,15 +7,15 @@ import java.util.Dictionary
 fun main() {
     val file = File("dictionary.json")
     val content = file.readText().trim()
-    val dictionary = Json.decodeFromString<List<Entry>>(content)
+    val dictionary = Json.decodeFromString<Map<String, String>>(content)
     showDescription(dictionary)
 }
 
-fun showDescription(dictionary: List<Entry>) {
+fun showDescription(dictionary: Map<String, String>) {
     while (true) {
         println("enter value or 0")
         val value = readln().lowercase()
         if (value == "0") break
-        dictionary.find { it.value == value }?.let { println(it.description) } ?: println("not found")
+        dictionary[value]?.let { println(it) } ?: println("not found")
     }
 }
